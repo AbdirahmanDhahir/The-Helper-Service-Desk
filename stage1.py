99958adb786188de8ba3c2f452ae91ae7858aafb
AbdirahmanDhahir <43729425+AbdirahmanDhahir@users.noreply.github.com>
2018-11-03 13:42:40 +0000

    first stage on the project
    The this the first stage of the project, in which i created a simple python programe idenfying the helper desk

--- stage1.py
+++ stage1.py
@@ -2,81 +2,108 @@
 #1. print hello
 #2. ask for name
 #3. choice a problem
-#----------------Global Variables--------------------------------
-#name
-#email
-#address
+#----------database---------------------------------------
+#creating a dictionary forum to send to the data basestring
+
+database = {}
+#--------------printing dictionaries------------------------
+def print_dictionary(dicts):
+	for key, value in dicts.items():
+		print key, ": ", value
+	
+#----------------ticket number --------------------------------
+import random
+
+value = random.randint(1, 100)
+print (value, "Your Ticket Number!")
 #----------------Dictionarys---------------------------------------
-problem_choice = {
+import collections
+problem_choice_noorder = {
 	'option 1': 'The screen is frozen',
 	'option 2': 'computer is slow',
 	'option 3': 'strange noises',
 }
-location_choice = {
+problem_choice = collections.OrderedDict(sorted(problem_choice_noorder.items()))
+
+location_choice_noorder = {
 	'option 1': 'Marketing',
 	'option 2': 'Sales',
 	'option 3': 'Human Resource',
 	'option 4': 'Accounting',
 	'option 5': 'CEO',
 }
-critical_level = {
+location_choice = collections.OrderedDict(sorted(location_choice_noorder.items()))
+critical_level_noorder = {
 	'level 1': 'ok',
 	'level 2': 'bad',
 	'level 3': 'very bad',
 	'level 4': 'immediate help',
 }
+critical_level = collections.OrderedDict(sorted(critical_level_noorder.items()))
 
 def intro():
 	print "Hello user, welcome to your help desk."
 	print "We will ask you a few personal questions about your self."
-	
+
+print "-"*50
 def user_name():
 	print "Please enter your name."
 	# global allows you to reference a varible from outside the funcation 
 	global name
 	# once you define the global varibale you can ask for a equal the name and recall it anywhere in the code. 
 	name = raw_input("> ")
+	#adding user name to our dictory form to send to the database
+	#to do this 
+	#dictory_name[new key to instert] = new value for the key 
+	# check below
+	database["name"] = name 
 	
+	print "-"*50	
+
 def user_email():
 	print "Please enter your email address."
 	global email
 	email = raw_input("> ")
-	
+	database["email"] = email
+	print "-"*50	
+
 def user_location():
 	print "From the list below, Please section your department."
-	for key,value in location_choice.items():
-		print(key,value)
+	print("DEBUG TEST")
+	print_dictionary(location_choice)
 	global location
 	location = raw_input("> ")
+	database["location"] = location
 	
+	print "-"*50	
+
 def issue_level():
 	print "Please select from the list below how critical is your problem."
-	for key,value in critical_level.items():
-		print(key,value)
+	print_dictionary(critical_level)
 	global level
 	level = raw_input("> ")
-	
-	
+	database["level"] = level
+		
+	print"-"*50	
+
 def problem_selection():
 	print "Please select the number referring to your problem."
 	#for python to display the options in the dictory we most select
 	#the key and vlaue. for example option 1 is a 'key' and the 'value' is screen frozen
 	#so to print this we tell python for key and value in the dictory above	
-	#the important thing is to use 'items()' and then print both.
-	for key,value in problem_choice.items():
-		print(key, value)
+	#the important thing is to use 'items and then print both.
+	print_dictionary(problem_choice)
 	global problem
 	problem = raw_input("> ")
+	database["problem"] = problem
+	print "_"*50
 	
-def result():
-print "Blow is summry of the issue you are having."
-global name
-global email
-global location
-global level 
-global problem
-	
-	
+def print_database():
+	print "Below is the information you have entered, please check if its correct?"
+	database_ordered = collections.OrderedDict(sorted(database.items()))
+	print_dictionary(database_ordered)
+	print "_"*50
+
 def start():
 	intro()
 	user_name()
@@ -84,7 +111,10 @@ def start():
 	user_location()
 	issue_level()
 	problem_selection()
-	result()
+	print_database()
+
+
+	
 	
 	
 start()
